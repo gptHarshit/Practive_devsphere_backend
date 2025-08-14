add9e78aa87a4e7fcad35af65fee4d7cef056830
Harsh <gupta.harshit0699@gmail.com>
2025-08-15 00:42:38 +0530

    Revised the concept of middleware

--- src/app.js
+++ src/app.js
@@ -2,21 +2,62 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("hello , i am first");
-    next();
-  },
-  (req, res, next) => {
-    console.log("hello , i am Second");
-    next();
-  },
-  (req, res) => {
-    console.log("hello , i am third");
-    res.send("hello this is the response from third route handler");
-  }
-);
+
+
+
+
+
+
+
+
+// app.use("/", (req, res, next) => {
+//   next();
+// });
+// app.get(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Middleware 1");
+//     next();
+//   },
+//   (req, res,next) => {
+//     console.log("Middleware 2");
+//    // res.send("done");
+//    next();
+//   }
+// );
+// app.get(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Middleware 1");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Middleware 2");
+//     res.send("done");
+//   }
+// );
+
+
+
+// app.get("/user1",(req,res)=>{
+//     console.log("Middleware 3");
+//     res.send("finally reached");
+// });
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("hello , i am first");
+//     next();
+//   }
+// );
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("hello , i am first");
+//     res.send("done");
+//   }
+// );
 
 // app.get("/user1/:userId/:username/:userage", (req,res) => {
 //     console.log(req.params);
