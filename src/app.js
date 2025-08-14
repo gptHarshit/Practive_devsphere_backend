9b579f2eb8d1fe27b924777e4016669bed30a8fb
Harsh <gupta.harshit0699@gmail.com>
2025-08-14 15:22:58 +0530

    Understood the route and its sequenc

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/test/math/1",(req,res) => {
+    res.send("Hello fro the server ?????????");
+});
+
 app.use("/test/math",(req,res) => {
     res.send("math");
 });
@@ -10,13 +14,7 @@ app.use("/robin",(req,res) => {
     res.send("Hello from Robin");
 });
 
-app.use("/love",(req,res) => {
-    res.send("Hello from Duniya");
-});
 
-app.use("/",(req,res) => {
-    res.send("Hello fro the server ?????????");
-});
 
 app.listen(3000,() => {
     console.log("Server is Successfully listening on PORT : 3000");
