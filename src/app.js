3d82e98a7eab95d220b7cce8e6a3689bb22cd194
Harsh <gupta.harshit0699@gmail.com>
2025-08-14 14:56:14 +0530

    install the express and created the server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test/math",(req,res) => {
+    res.send("math");
+});
+
+app.use("/robin",(req,res) => {
+    res.send("Hello from Robin");
+});
+
+app.use("/love",(req,res) => {
+    res.send("Hello from Duniya");
+});
+
+app.use("/",(req,res) => {
+    res.send("Hello fro the server ?????????");
+});
+
+app.listen(3000,() => {
+    console.log("Server is Successfully listening on PORT : 3000");
+});
