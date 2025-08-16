61849ba928408a6543605b857bf5908b634033b6
Harsh <gupta.harshit0699@gmail.com>
2025-08-16 22:44:01 +0530

    Added the User into the data base by using the signup API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const User = require("./models/user")
 
 const app = express();
+
+app.post("/signup",async (req,res)=> {
+    const userObj = {
+        firstName : "Anmol",
+        lastName : "Gupta",
+        emailId : "anmol123@gmail.com",
+        password :  "anmol006"
+    };
+
+    const user = new User(userObj);
+    await user.save();
+    res.send("User Added Successfully!!");
+})
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://codehelp:WhoUzHIbC6bAQ6qe@cluster0.czv9bds.mongodb.net/"
+    "mongodb+srv://codehelp:WhoUzHIbC6bAQ6qe@cluster0.czv9bds.mongodb.net/testing_1"
   );
 };
 
