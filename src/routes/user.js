8918ba8ddd0f2fe6e438144d346eaba506d1f1a9
Harsh <gupta.harshit0699@gmail.com>
2025-08-25 22:01:35 +0530

    Made user request and user connection API

--- src/routes/user.js
+++ src/routes/user.js
@@ -20,10 +20,41 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
         "about",
         "gender",
       ]);
-  //  console.log(data);
+    //  console.log(data);
     res.json({ message: "Connection fetched successfully", data });
   } catch (error) {
     res.status(400).send("ERROR : " + error.message);
+  }
+});
+
+// a bug can be in this api ,be carefull if any bug appear so make sure to check this API
+userRouter.get("/user/connection", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const Connection = await connectionRequest
+      .find({
+        $or: [
+          { toUserId: loggedInUser._id, status: "accepted" },
+          { fromUserId: loggedInUser._id, status: "accepted" },
+        ],
+      })
+      .populate("fromUserId", [
+        "firstName",
+        "lastName",
+        "photoUrl",
+        "age",
+        "about",
+        "gender",
+        "skills",
+      ]);
+    // console.log(Connection);
+    const data = Connection.map((row) => row.fromUserId);
+    res.json({
+      message: "Connection that " + loggedInUser.firstName + " have are :- ",
+      data,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR : - " + error.message);
   }
 });
 
