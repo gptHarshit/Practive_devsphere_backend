d9fa171784d271ab1ed33cfc49249ca7cf958e15
Harsh <gupta.harshit0699@gmail.com>
2025-09-30 19:25:54 +0530

    successfully written the login to store the data in the database

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const { Chat } = require("../models/chat");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -22,17 +23,37 @@ const initializeSocket = (server) => {
       socket.join(roomId);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      console.log(firstName + "  " + text);
-      // saving mess to the database
-      try {
-        
-      } catch (error) {
-        console.log(error);
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, targetUserId, text }) => {
+        // saving mess to the database
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+          console.log(firstName + "  " + text);
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          await chat.save();
+
+          io.to(roomId).emit("messageReceived", { firstName, text });
+        } catch (error) {
+          console.log(error);
+        }
       }
-      io.to(roomId).emit("messageReceived", { firstName, text });
-    });
+    );
 
     socket.on("disconnect", () => {});
   });
